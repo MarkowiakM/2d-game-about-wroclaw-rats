6c3e878b90be068f26bbd80eddc28a9188fc6cdc
Paulina <pjlobocka@gmail.com>
2024-04-29 10:47:41 +0200

    music and pause settings

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -15,6 +15,9 @@ public class GameStateManager : MonoBehaviour
     public static int[] unlockedLevels;
     public static GameStateManager instance;
 
+    private static int isGameOn = 1;
+    private static int isMusicOn = 1;
+
     void Awake()
     {
         if (instance == null)
@@ -55,5 +58,30 @@ public class GameStateManager : MonoBehaviour
     {
         // level 1 is at index 0
         unlockedLevels[levelIndex-2] = 1;
+    }
+
+    public static void StopResumeGame(int isGameOn) {
+        if (isGameOn == 1)
+        {
+            Time.timeScale = 0f;
+            isGameOn = 0;
+        } else
+        {
+            Time.timeScale = 1f;
+            isGameOn = 1;
+        }
+    }
+
+    public static void PlayPasueMusic(int isMusicOn)
+    {
+        if (isMusicOn == 0)
+        {
+            instance.musicSource.Pause();
+            instance.effectAudioSource.mute = true;
+        } else
+        {
+            instance.musicSource.Play();
+            instance.effectAudioSource.mute = false;
+        }
     }
 }
